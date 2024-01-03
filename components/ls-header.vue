82e93d6ee74d82289ac5d27f8f90caf89bf84544
Niklas <niklas.lorenz@niklasl.com>
2024-01-03 21:32:54 +0100

    first tests with nuxt

---
+++ components/ls-header.vue
@@ -0,0 +1,17 @@
+<script setup lang="ts">
+
+</script>
+
+<template>
+  <div>
+    <div class="header">
+      <div class="header_title">
+        <h1>Inventory Tool</h1>
+      </div>
+    </div>
+  </div>
+</template>
+
+<style scoped>
+
+</style>
